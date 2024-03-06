622126a29bbb235ec5e5641fb0094e659b3c5e02
Semyon Grigoriev <haitaks@yandex.ru>
2024-03-06 12:29:25 +0300

    added review validation

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,22 @@
 from rest_framework import serializers
-from reviews.models import Review, Comment
+from reviews.models import Comment, Genre, Review, Title
 
+class TitleSerializer(serializers.ModelSerializer):
+    name = serializers.StringRelatedField()
+    year = serializers.DateField()
+    description = serializers.StringField()
+    genre = serializers.SlugRelatedField()
+    category = serializers.SlugRelatedField()
+
+    rating = serializers.SerializerMethodField(default=None)
+       
+    def get_rating(self, obj):
+        return self.objects.objects.annotate(
+        rating=Avg('reviews__score')).order_by('-year')
+    
+    class Meta:
+        model = Title
+        fields = ("id", "name", "year", "rating", "description", "genre", "category")
 
 class ReviewSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
@@ -8,11 +24,25 @@ class ReviewSerializer(serializers.ModelSerializer):
         slug_field="username",
         default=serializers.CurrentUserDefault(),
     )
-    title = serializers.PrimaryKeyRelatedField(read_only=True)
-
+    
+    def validate(self, attrs):
+        if not self.context['request'].method == 'POST':
+            return attrs
+        if Review.objects.filter(
+            title_id=self.context['view'].kwargs.get('title_id'),
+            author=self.context['request'].user,
+        ).exists():
+            raise serializers.ValidationError(
+                (
+                    'Пользователь может оставлять отзыв на каждое произведение'
+                    'не более одного раза'
+                )
+            )
+        return attrs
+    
     class Meta:
         model = Review
-        fields = ("id", "author", "score", "pub_date")
+        fields = ("id","text", "author", "score", "pub_date")
 
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
@@ -20,8 +50,8 @@ class CommentSerializer(serializers.ModelSerializer):
         slug_field="username",
         default=serializers.CurrentUserDefault(),
     )
-    review = serializers.PrimaryKeyRelatedField(read_only=True)
-
+    
     class Meta:
         model = Comment
         fields = ("id","text", "author", "pub_date")
+    
