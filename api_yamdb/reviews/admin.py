6bf32f59f3f37996da09933ce59d6fac68d31239
Semyon Grigoriev <haitaks@yandex.ru>
2024-03-06 13:53:20 +0300

    added models, admin

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,6 +1,49 @@
 from django.contrib import admin
-# from .models import Comment, Review, Title
+from .models import Category, Comment, Genre, Review, Title
 
-# admin.site.register(Review)
-# admin.site.register(Comment)
-# admin.site.register(Title)
+from django.contrib import admin
+
+from .models import Category, Genre, Title
+
+admin.site.empty_value_display = 'Не задано'
+
+
+@admin.register(Title)
+class TitleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'category')
+    list_editable = ('description', 'category')
+    search_fields = ('name',)
+    list_display_links = ('name',)
+    list_filter = ('genre', 'category')
+    filter_horizontal = ('genre',)
+    list_per_page = 10
+
+
+class TitleInline(admin.StackedInline):
+    model = Title
+    extra = 0
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = (TitleInline,)
+    list_display = ('name', 'slug')
+    list_display_links = ('name',)
+    search_fields = ('slug',)
+    list_per_page = 10
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    list_display_links = ('name',)
+    search_fields = ('slug',)
+    list_per_page = 10
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    pass
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,126 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+# создала пока здесь после создания приложения user добавить импорт, убрать
+# класс, не забыть поменять в settings AUTH_USER_MODEL(зарегать в админке?)
+class User(AbstractUser):
+    pass
+
+
+class PublishedModel(models.Model):
+    name = models.CharField(verbose_name='Название', max_length=256)
+    slug = models.SlugField(unique=True, max_length=50)
+    # возможно стоит поменять на слаг?
+    def __str__(self):
+        return self.name
+    class Meta:
+        abstract = True
+
+
+class Category(PublishedModel):
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Genre(PublishedModel):
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+
+class Title(models.Model):
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=256,
+        help_text='Название произведения, не более 256 символов'
+    )
+    year = models.PositiveIntegerField(
+        verbose_name='Год выпуска',
+        validators=[MaxValueValidator(timezone.now().year)],
+        help_text='Год выпуска произведения (не может быть больше текущего).'
+    )
+    description = models.TextField(verbose_name='Описание', blank=True)
+    # Здесь возможно стоит делать через отдельную модель через атрибут through
+    # но это не точно =)
+    genre = models.ManyToManyField(
+        Genre,
+        verbose_name='Жанр',
+        blank=True,
+        # null=True,
+        related_name='titles',
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        verbose_name='Категория',
+        null=True,
+        blank=True,
+        related_name='titles'
+    )
+
+    class Meta:
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    text = models.TextField(verbose_name='Текст отзыва')
+    score = models.IntegerField(
+        verbose_name='Оценка',
+        validators=[MinValueValidator(1), MaxValueValidator(10)],
+        help_text='Минимальная оценка - 1, максимальная оценка - 10'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Автор отзыва',
+        related_name='reviews',
+    )
+    pub_date = models.DateTimeField(
+        verbose_name='Дата и время отзыва',
+        auto_now_add=True
+    )
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        verbose_name='Произведение',
+        related_name='reviews',
+        # unique=True
+    )
+
+    # не уверена, что это нужно, но все же
+    def __str__(self):
+        return self.text[:15]
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+        ordering = ['pub_date']
+
+
+class Comment(models.Model):
+    text = models.TextField(verbose_name='Текст комментария')
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Автор отзыва',
+        related_name='comments',
+    )
+    pub_date = models.DateTimeField(
+        verbose_name='Дата и время комментария',
+        auto_now_add=True
+    )
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        verbose_name='Отзыв',
+        null=True,
+        related_name='comments'
+    )
