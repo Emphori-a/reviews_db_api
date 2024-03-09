a9c30850b952896bc325f789ccf4c114e652c7c6
Valery Martynova <emphoria@yandex.ru>
2024-03-09 13:42:59 +0300

    users model, user registration

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -6,9 +6,9 @@ class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (request.method in permissions.SAFE_METHODS
                 or (request.user.is_authenticated
-                    and request.user.role == 'admin'))
+                    and request.user.is_admin()))
 
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
                 or (request.user.is_authenticated
-                    and request.user.role == 'admin'))
+                    and request.user.is_admin()))
--- api_yamdb/user/serializers.py
+++ api_yamdb/user/serializers.py
@@ -1,31 +1,52 @@
 import re
 
-from rest_framework import serializers, status
-
 from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
 
 User = get_user_model()
-
-
-class RegexValidator:
-    pass
 
 
 class UserSignupSerializer(serializers.Serializer):
     username = serializers.CharField(max_length=150)
     email = serializers.EmailField(max_length=254)
 
+    def validate_username(self, username):
+        if username == 'me':
+            raise serializers.ValidationError(
+                'Недопустимое имя пользователя.'
+            )
+        if not re.match(r'^[\w.@+-]+\Z', username):
+            raise serializers.ValidationError(
+                ('Имя пользователя может содержать только буквы, '
+                 'цифры и знаки @ / . / + / - / _')
+            )
+        return username
+
+
 class ConfirmationCodeSerializer(serializers.Serializer):
     username = serializers.CharField(max_length=254)
     confirmation_code = serializers.CharField()
 
 
-class UserProfileSerializer(serializers.Serializer):
-    first_name = serializers.CharField(max_length=150)
-
+class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ('username', 'email', 'first_name',
+                  'last_name', 'bio', 'role')
+        lookup_field = 'username'
+        extra_kwargs = {
+            'url': {'lookup_field': 'username'}
+        }
 
-    def create(self, validated_data):
-        return User.objects.create(**validated_data)
+    def validate_username(self, username):
+        if username == 'me':
+            raise serializers.ValidationError(
+                'Недопустимое имя пользователя.'
+            )
+        if not re.match(r'^[\w.@+-]+\Z', username):
+            raise serializers.ValidationError(
+                ('Имя пользователя может содержать только буквы, '
+                 'цифры и знаки @ / . / + / - / _')
+            )
+        return username
