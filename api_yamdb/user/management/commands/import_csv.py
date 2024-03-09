7cc8b8ab0102aea3254c2c88abf33923c1d9265e
Thinker <lexa.thinker@gmail.com>
2024-03-09 23:42:31 +0500

    сделал команду python .\api_yamdb\manage.py import_csv .\api_yamdb\static\data\
    она мне не нравится, но работает

--- api_yamdb/user/management/commands/import_csv.py
+++ api_yamdb/user/management/commands/import_csv.py
@@ -1,7 +1,8 @@
-import csv
+from csv import DictReader
+from pathlib import Path
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
-from django.core.exceptions import ValidationError
+from reviews.models import Comment, Category, Review, Title, Genre
 
 User = get_user_model()
 
@@ -10,30 +11,75 @@ class Command(BaseCommand):
     help = 'Imports data from a CSV file'
 
     def add_arguments(self, parser):
-        parser.add_argument('csv_file', type=str)
+        parser.add_argument('csv_directory', type=str)
 
-    def handle(self, *args, **kwargs):
-        csv_file = kwargs['csv_file']
+    def handle(self, *args, **options):
+        csv_directory = options['csv_directory']
 
-        with open(csv_file, 'r') as f:
-            reader = csv.reader(f)
-            next(reader, None)
-            for row in reader:
-                try:
-                    user = User(
-                        id=int(row[0]),
-                        username=row[1],
-                        email=row[2],
-                        role=row[3],
-                        bio=row[4],
-                        first_name=row[5],
-                        last_name=row[6])
-                    user.set_unusable_password()
-                    user.full_clean()
-                    user.save()
-                except ValidationError as e:
-                    self.stdout.write(self.style.WARNING(
-                        f"Skipping invalid data for user with id={row[0]}: {e}"
-                    ))
+        for row in DictReader(open(Path(csv_directory) / 'users.csv',
+                                   encoding='utf-8')):
+            user = User(id=row['id'],
+                        username=row['username'],
+                        email=row['email'],
+                        role=row['role'],
+                        bio=row['bio'],
+                        first_name=row['first_name'],
+                        last_name=row['last_name'])
+            user.save()
+        self.stdout.write(self.style.SUCCESS('User выполнен успешно'))
 
-        self.stdout.write(self.style.SUCCESS('Импорт данных выполнен успешно'))
+        for row in DictReader(open(Path(csv_directory) / 'category.csv',
+                                   encoding='utf-8')):
+            category = Category(id=row['id'],
+                                name=row['name'],
+                                slug=row['slug'])
+            category.save()
+        self.stdout.write(self.style.SUCCESS('Category выполнен успешно'))
+
+        for row in DictReader(open(Path(csv_directory) / 'genre.csv',
+                                   encoding='utf-8')):
+            genre = Genre(id=row['id'],
+                          name=row['name'],
+                          slug=row['slug'])
+            genre.save()
+        self.stdout.write(self.style.SUCCESS('Genre выполнен успешно'))
+
+        for row in DictReader(open(Path(csv_directory) / 'titles.csv',
+                                   encoding='utf-8')):
+            title = Title(id=row['id'],
+                          name=row['name'],
+                          year=row['year'],
+                          category=Category.objects.get(id=row['category']))
+            title.save()
+        self.stdout.write(self.style.SUCCESS('Title выполнен успешно'))
+
+        for row in DictReader(open(Path(csv_directory) / 'review.csv',
+                                   encoding='utf-8')):
+            review = Review(id=row['id'],
+                            title=Title.objects.get(id=row['title_id']),
+                            text=row['text'],
+                            author=User.objects.get(id=row['author']),
+                            score=row['score'],
+                            pub_date=row['pub_date'])
+            review.save()
+        self.stdout.write(self.style.SUCCESS('Review выполнен успешно'))
+
+        for row in DictReader(open(Path(csv_directory) / 'comments.csv',
+                                   encoding='utf-8')):
+            comment = Comment(id=row['id'],
+                              review=Review.objects.get(id=row['review_id']),
+                              text=row['text'],
+                              author=User.objects.get(id=row['author']),
+                              pub_date=row['pub_date'])
+            comment.save()
+        self.stdout.write(self.style.SUCCESS('Comment выполнен успешно'))
+
+        for row in DictReader(open(Path(csv_directory) / 'genre_title.csv',
+                                   encoding='utf-8')):
+            title = Title.objects.get(pk=row['title_id'])
+            genre = Genre.objects.get(pk=row['genre_id'])
+            title.genre.add(genre)
+            comment.save()
+        self.stdout.write(self.style.SUCCESS('genre_title выполнен успешно'))
+
+        self.stdout.write(self.style.SUCCESS('Импорт данных выполнен!'))
