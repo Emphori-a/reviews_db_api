ec4fa88142ea5f8d067f378138dc38283fcfec41
Semyon Grigoriev <haitaks@yandex.ru>
2024-03-11 11:17:53 +0300

    full rework for permissions

--- api_yamdb/api/v1/permissions.py
+++ api_yamdb/api/v1/permissions.py
@@ -20,3 +20,17 @@ class IsModeratorIsAdminIsAuthorOrReadOnly(
                     or (obj.author == request.user)
                 ))
                 or request.method in permissions.SAFE_METHODS)
+
+class IsOwnerOrIsAdmin(permissions.IsAdminUser):
+
+    def has_permission(self, request, view):
+        return (request.user.is_authenticated
+                and (request.user.is_admin()
+                     or request.user.is_superuser)
+                )
+
+    def has_object_permission(self, request, view, obj):
+        return ((request.user.is_authenticated
+                 and (request.user.is_admin()
+                      or request.user.is_superuser))
+                or obj.author == request.user)
