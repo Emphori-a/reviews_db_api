0556b72d9a258be7a863b9119b788c9811b337db
Valery Martynova <emphoria@yandex.ru>
2024-03-11 08:59:29 +0300

    create api/v1 directory

---
+++ api_yamdb/api/v1/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from .views import (CategoryViewSet, CommentViewSet,
+                    GenreViewSet, ReviewViewSet, TitleViewSet)
+
+
+router_v1 = DefaultRouter()
+router_v1.register('titles', TitleViewSet, basename='titles')
+router_v1.register('categories', CategoryViewSet, basename='categories')
+router_v1.register('genres', GenreViewSet, basename='genres')
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet,
+    basename='review'
+)
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet,
+    basename='comment'
+)
+urlpatterns = [
+    path('', include(router_v1.urls)),
+]
